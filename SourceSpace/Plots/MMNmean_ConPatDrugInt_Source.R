21a42eebd83f0bd5c475bd28ad5704b1e4cbbd91
Alistair Perry <alistairperry@alistairsnewmbp.home>
2022-07-13 22:54:40 +0100

    update script cleaning

--- SourceSpace/Plots/MMNmean_ConPatDrugInt_Source.R
+++ SourceSpace/Plots/MMNmean_ConPatDrugInt_Source.R
@@ -1,11 +1,12 @@
 
 # Setup -------------------------------------------------------------------
 
-#Working directory and paths
+# Setup working directory and paths
 
 setwd("/Users/alistairperry/Documents/Cambridge/Project/Analysis/LFPs/newmaxfilter_icafixes/LFPs_COH_wrad_allMEGch_wfids/C_Plots/DrugAnalysis")
 
-#Load packages
+# The following packages are required
+
 library("plyr")
 library("lattice")
 library("ggplot2")
@@ -15,7 +16,6 @@ library("rmarkdown")
 library("Rmisc")
 library("devtools")
 library("gghalves")
-
 library("readxl")
 
 # width and height variables for saved plots - leave it as default for now
@@ -23,15 +23,16 @@ library("readxl")
 w = 4
 h = 3
 
-
-# Set fig output dir
+# Set figure output dir
 
 FigOutDir <- "/Users/alistairperry/Documents/Cambridge/Project/Analysis/LFPs/newmaxfilter_icafixes/LFPs_COH_wrad_allMEGch_wfids/C_Plots/DrugAnalysis/MeanPlots/"
 
 dir.create(FigOutDir)
 
 
-# Data Table
+# Text and ROI names for reading, writing files
+
+basefname = "commconpatplac_remo_wbothsess_noc22_LFP_"
 
 ROInames = c("RIFG", "RSTG", "RAUD")
 
@@ -39,35 +40,39 @@ Labnames = c("R IFG", "R STG", "R AUD")
 
 
 
+# Drug Group Interaction Setup --------------------------------------------------------
 
-# Drug Group Interaction --------------------------------------------------------
+# Loop through each region in for loop
 
-#Loop through each region
-
-#rep3 standard
+# Pickup text file
 
 
 for (i in seq(1,3)) {
   
-datfname <- paste("commconpatplac_remo_wbothsess_noc22_LFP_", ROInames[i], "_MMNmean_rep3_ConsandPats_DrugInt_fullsample_forJASP.txt", sep="")
+datfname <- paste(basefname, ROInames[i], "_MMNmean_rep3_ConsandPats_DrugInt_fullsample_forJASP.txt", sep="")
+
+
+# Read file
 
 MMNtab <- read.delim(datfname)
 
+
+# Split into cons and pats
 
 CN <- subset(MMNtab, Group == 1)
 
 Pat <- subset(MMNtab, Group == 2)
 
 
-
-# Start variables
-
+# And also drug session
 
 CN1dat <- CN$meanMMNcol_PLA
 CN2dat <- CN$meanMMNcol_MEM
 Pat1dat <- Pat$meanMMNcol_PLA
 Pat2dat <- Pat$meanMMNcol_MEM
 
+
+# Setup metadata for plotting
 
 ncnt <- length(CN1dat)
 npat <- length(Pat1dat)
@@ -83,15 +88,18 @@ x1[(ncntscans+1):totalscans] <- rep(c(3,4), each=npat)
 cntids <- rep(seq(1,ncnt),2)
 patids <- rep(seq(ncnt+1,ncnt+npat),2)
 
+
+# Now compile into appropriate dataframe
+
 d <- data.frame(y = c(CN1dat, CN2dat, Pat1dat, Pat2dat), x = x1, ID_Order = as.factor(c(cntids, patids)))
 
 y_lim_min <- min(d$y)
 y_lim_max <- max(d$y)
 
 
-#Descriptive stats for calculating and connecting group means
+# Descriptive stats for calculating and connecting group means
 
-#First do the descriptives
+# First do the descriptives
 
 group <- c(1,2,1,2)
 
@@ -104,10 +112,11 @@ score_mean <- c(score_mean_1, score_mean_2, score_mean_3, score_mean_4)
 
 
 #### get confidence intervals
-score_ci_1 <- CI(CN1dat, ci = 0.95) #CN1
-score_ci_2 <- CI(CN2dat, ci = 0.95) #CN1
-score_ci_3 <- CI(Pat1dat, ci = 0.95) #CN1
-score_ci_4 <- CI(Pat2dat, ci = 0.95) #CN1
+score_ci_1 <- CI(CN1dat, ci = 0.95) #i.e CN1
+score_ci_2 <- CI(CN2dat, ci = 0.95) #CN2
+score_ci_3 <- CI(Pat1dat, ci = 0.95) #PAT1
+score_ci_4 <- CI(Pat2dat, ci = 0.95) #PAT2
+
 
 # CI's around mean
 ci <- c((score_ci_1[1] - score_ci_1[3]), (score_ci_2[1] - score_ci_2[3]), (score_ci_3[1] - score_ci_3[3]), (score_ci_4[1] - score_ci_4[3]))
@@ -115,11 +124,11 @@ ci <- c((score_ci_1[1] - score_ci_1[3]), (score_ci_2[1] - score_ci_2[3]), (score
 summary_df <- data.frame(group, score_mean, ci)
 
 
-
-#First we must again define the x-coordinates of the means.
+# First we must again define the x-coordinates of the means.
 x_tick_means_x <- c(1.15, 1.85) #same as above
 
-#Start plotting
+
+# Jitter to space datapoints in plot
 
 set.seed(321)
 
@@ -130,16 +139,19 @@ x1fk[(ncntscans+1):totalscans] <- rep(x_tick_means_x, each=npat)
 
 xj <- jitter(x1fk, amount = .09)
 
-#xj <- jitter(x1fk)
-
 d$xj <- xj
 
 d$x_tick_means_x <- as.factor(x_tick_means_x)
 
 
+# ggplot: Drug Group Interaction ------------------------------------------
+
+
 f1 <- ggplot(data = d, aes(y = y)) +
   
   #Add geom_() objects
+  
+  #All data points
   
   geom_point(data = d %>% filter(x =="3"), aes(x = xj), color = 'red', size = 1.5,alpha = .6) +
   geom_point(data = d %>% filter(x =="4"), aes(x = xj), color = 'red', size = 1.5,alpha = .6) +
@@ -147,11 +159,15 @@ f1 <- ggplot(data = d, aes(y = y)) +
   geom_point(data = d %>% filter(x =="1"), aes(x = xj), color = 'blue', size = 1.5, alpha = .6) +
   geom_point(data = d %>% filter(x =="2"), aes(x = xj), color = 'blue', size = 1.5, alpha = .6) +
   
+  
+  #Lines connecting each individual across drug condition
+  
   geom_line(data = d %>% filter(x =="3" | x =="4"), aes(x = xj, group = ID_Order), color = 'red', alpha = 0.2) +
   
   geom_line(data = d %>% filter(x =="1" | x =="2"), aes(x = xj, group = ID_Order), color = 'blue', alpha = 0.2) +
   
  
+  #Axis labels
   
   scale_x_continuous(breaks=x_tick_means_x, labels=c("PLA", "MEM"))+
   
@@ -165,6 +181,8 @@ f1 <- ggplot(data = d, aes(y = y)) +
   
   
   
+  #Font and size of axis labels
+  
   theme(plot.title = element_text(face="bold", size = 14, hjust = 0.5, color = "black"), axis.title.y = element_text(face="bold", size = 11), axis.title.x = element_text(face="bold", size = 12, color = "black")) + 
   
   theme(axis.text.x = element_text(face="bold", size = 12, color = "black"), axis.text.y = element_text(face="bold", size = 11, color = "black")) +
@@ -177,7 +195,7 @@ f1 <- ggplot(data = d, aes(y = y)) +
   coord_cartesian(ylim=c(y_lim_min, y_lim_max)) +
   
   
-  #Add lines connecting the two means
+  #Add thick lines connecting the means of groups as function of drug condition 
   
   geom_line(data = summary_df[3:4,], aes(x = x_tick_means_x, y = score_mean),
             color = 'red', size = 1.5, alpha = 1) + 
@@ -186,7 +204,7 @@ f1 <- ggplot(data = d, aes(y = y)) +
             color = 'blue', size = 1.5, alpha = 1) +
   
   
-  # means with shape = 16, filled circle
+  #And error bars (again both groups as function of drug condition)
   geom_errorbar(data = d %>% filter(x=="1"), aes(x = 1, y = score_mean[1], ymin = score_mean[1]-ci[1], ymax = score_mean[1]+ci[1]),
                 position = position_nudge(0.15),
                 color = "blue", width = 0.10, size = 0.8, alpha = 0.8) +
@@ -218,17 +236,22 @@ f1 <- ggplot(data = d, aes(y = y)) +
   geom_point(data = d %>% filter(x=="4"), aes(x = 2, y = score_mean[4]),
              position = position_nudge(x = -0.15), shape = 21, color = "black", fill = "red",  size = 3, alpha = 0.8)
 
+
+# Write final plot out
+
 f1
+
+
+# And save
 
 figfname<-paste(FigOutDir, '/LFP_MMN3_ConPatDrugInt_wfig_', ROInames[i], '.tiff', sep="")
 ggsave(figfname, width = w, height = h)
 
 
-}
+} # Loop ends
 
 
-
-# 3 Groups: Drug Pat Subgroup Interaction ---------------------------------
+# 3 Groups: Drug Pat Subgroup Interaction Setup ---------------------------------
 
 #rep3
 
